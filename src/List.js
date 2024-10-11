b8145df8970c03ad610984766e92d762731b4456
luka-danger <lukathevegan@gmail.com>
2024-10-11 09:32:57 -0600

    Create List class
    Add constructor and toggleCompleted() funciton

--- src/List.js
+++ src/List.js
@@ -1,7 +1,12 @@
 export default class List {
-    constructor(title, description, dueDate) {
+    constructor(title, description, dueDate, hasCompleted) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
+        this.hasCompleted = hasCompleted;
+    }
+
+    toggleCompleted(index) {
+        this.hasCompleted = !this.hasCompleted;
     }
 }
