4f270aa4fc9fad3ef22669d437df7fec589d052c
luka-danger <lukathevegan@gmail.com>
2024-10-15 14:35:12 -0600

    Add styling effects

--- src/Render.js
+++ src/Render.js
@@ -12,11 +12,13 @@ export function render() {
         listElement.setAttribute("class", "list-card");
         listElement.innerHTML = `
         <div class="list-header">
-            <h3 class="title">${listItem.title}</h3>
+            <h3 class="title ${listItem.hasCompleted ? 'completed' : ''}">${listItem.title}</h3>
         </div>
         <div class="list-body">
-            <p class="description">${listItem.description}</p>
+            <p class="description ${listItem.hasCompleted ? 'complete-this' : ''}">${listItem.description}</p>
             <p class="complete-status ${listItem.hasCompleted ? 'complete' : 'incomplete'}"> ${listItem.hasCompleted ? "Complete" : "Incomplete"}</p>
+        </div>
+        <div class="list-buttons">
             <button class="toggle-complete-btn" onclick="toggleComplete(${i})">
                 ${listItem.hasCompleted ? 'Make Incomplete' : 'Complete Task'}
             </button>
