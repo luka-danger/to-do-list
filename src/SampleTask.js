1372ce99cfa3b3ec244c23953eb5202efbadd8cc
luka-danger <lukathevegan@gmail.com>
2024-10-15 14:35:31 -0600

    Update Sample Tasks

--- src/SampleTask.js
+++ src/SampleTask.js
@@ -6,7 +6,7 @@ export function addSampleTasks() {
     const odinProject = new List("To Do List Project", "Finish Odin Project To Do List.", false);
     const dailyRun = new List("5 mile run", "Easy 5 with 4x30sec hill strides", false);
     const buildPlatform = new List("Build Platform", "Finish building deadlift platform", false);
-    const walkWaffles = new List("Walk Waffles", "Take the little man around the block for some exercise", true)
+    const walkWaffles = new List("Walk Waffles", "2-3 mile walk around the neighborhood", true)
 
     myList.push(odinProject)
     myList.push(dailyRun)
