79f3807271328308ee52844657f8ab2c230a2b35
luka-danger <lukathevegan@gmail.com>
2024-10-15 12:59:34 -0600

    Change To Do List to To Do List Project

--- src/SampleTask.js
+++ src/SampleTask.js
@@ -3,7 +3,7 @@ import { myList } from "./main";
 
 export function addSampleTasks() {
     // Add sample list items
-    const odinProject = new List("To Do List", "Finish Odin Project To Do List.", false);
+    const odinProject = new List("To Do List Project", "Finish Odin Project To Do List.", false);
     const dailyRun = new List("5 mile run", "Easy 5 with 4x30sec hill strides", false);
     const buildPlatform = new List("Build Platform", "Finish building deadlift platform", false);
     const walkWaffles = new List("Walk Waffles", "Take the little man around the block for some exercise", true)
