bc004ddeff2ff9e77b84378b0e7d75eb0860ee19
luka-danger <lukathevegan@gmail.com>
2024-10-15 18:26:07 -0600

    Update documentation

--- src/Clear.js
+++ src/Clear.js
@@ -2,11 +2,26 @@ import { myList } from "./main";
 import { render } from "./Render";
 import { clearMyList } from "./TaskManager";
 
+/* 
+Function: Clear To Do List
+
+Description: Accesses the HTML button "clear", clears the page and removes all items 
+from the myList array and from local storage, and and triggers render to dynamically
+update the page.
+
+Example: Clicking "Clear" will clear the screen. If you refresh the window, the 
+original sample tasks will be displayed on the window.
+*/
 export function clearList() {
     const clearButton = document.querySelector('#clear-task-btn');
     clearButton.addEventListener('click', () => {
+        // Empty local storage
         localStorage.removeItem('myList');
+
+        // Reset / Clear List Array
         clearMyList();
+
+        // Dynamically refresh the page
         render();
     });
 }
